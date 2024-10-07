b9cd72e2662d7d49a0a2e5a2a714269d2d6d304f
RSC Games <rsc.games.official@gmail.com>
2024-10-07 17:42:49 -0400

    One line assembler implemented.

--- scripts/encode_instruction.py
+++ scripts/encode_instruction.py
@@ -4,12 +4,13 @@ import sys
 
 
 class InstructionComponent():
-    def __init__(self, type: int, data):
+    def __init__(self, type: int, orig_val, encoded: int):
         self.type = type
-        self.data = data
+        self.orig_val = orig_val
+        self.encoded = encoded
 
     def __repr__(self):
-        return f"[type: {optypes.REVERSE_OP_LUT[self.type]}, data: {self.data}]"
+        return f"[type: {optypes.REVERSE_OP_LUT[self.type]}, val: {self.orig_val}, hex: {hex(self.encoded)}]"
 
 
 class Instruction():
@@ -35,20 +36,40 @@ def encode_instr(instr: str) -> int:
     
     comps = instr.split(" ")
     instr_name = comps[0].lower()
-    print(comps)
+
+    # In special cases, one instruction is an alias of another. Find these cases.
+    if instr_name in isa.INSTR_REENCODE_LUT:
+        instr = " ".join(isa.INSTR_REENCODE_LUT[instr_name])
+        comps = instr.split(" ")
+        instr_name = comps[0].lower()
 
     # Determine operand types
     op_types = instr_get_operand_types(comps)
+
+    if op_types is None:
+        return -1
+
     instr_opcode = encode_instr_opcode(instr_name, op_types)
 
+    if instr_opcode is None:
+        if isa.instruction_def_exists(instr_name):
+            print(f"ERROR: Unrecognized instruction/operand pair: {instr_name}, {isa.operands_as_str(op_types)}")
+            print("Valid instruction definitions listed below:")
+            isa.print_instruction_entries(instr_name)
+
+        else:
+            print(f"ERROR: Undefined instruction: {instr_name}")
+        return -1
+
     # Encoded in little endian form.
-    word = isa.encode_word()
-
-    return 0
+    return isa.encode_word(instr_opcode, op_types)
 
 
-def instr_get_operand_types(comps: list[str]) -> list[InstructionComponent]:
-    return [get_comp_type(comp) for comp in comps[1:]]
+def instr_get_operand_types(comps: list[str]) -> list[InstructionComponent] | None:
+    try:
+        return [get_comp_type(comp) for comp in comps[1:]]
+    except RuntimeError:
+        return None
 
 
 def get_comp_type(comp: str) -> InstructionComponent:
@@ -62,37 +83,42 @@ def get_comp_type(comp: str) -> InstructionComponent:
 
     # Is this a register ID or conditional?
     if comp in optypes.REGS_LIST:
-        print(f"Identified register {comp}")
-        return InstructionComponent(optypes.OPTYPE_REGISTER, comp)
+        #print(f"Identified register {comp}")
+        return InstructionComponent(optypes.OPTYPE_REGISTER, comp, optypes.REGS_LIST[comp])
     elif comp in optypes.COND_LIST:
-        print(f"Identified conditional {comp}")
-        return InstructionComponent(optypes.OPTYPE_CONDITIONAL, comp)
+        #print(f"Identified conditional {comp}")
+        return InstructionComponent(optypes.OPTYPE_CONDITIONAL, comp, optypes.COND_LIST[comp])
     
+    # TODO: Later add support for labels (in actual assembler)
     # Is this a hexadecimal value?
+    if comp[:2] != "0x":
+        print(f"ERROR: Unrecognized component {comp}")
+        raise RuntimeError("Unrecognized component.")
+
     potential_hex = comp[2:]
 
     try:
-        _ = binascii.unhexlify(potential_hex)
-        print(f"Identified address {comp}")
-        return InstructionComponent(optypes.OPTYPE_IMMEDIATE, comp)
+        val = binascii.unhexlify(potential_hex)
+        #print(f"Identified address {comp}")
+        return InstructionComponent(optypes.OPTYPE_IMMEDIATE, comp, int.from_bytes(val, 'big', signed=False))
     except:
-        print(f"ERROR: Unrecognized component {comp}")
+        print(f"ERROR: Illegal hex value: {comp}")
         raise RuntimeError("Unrecognized component.")
     
 
-def encode_instr_opcode(instr: str, optypes: list[InstructionComponent]) -> Instruction:
-    # TODO: Take the instr and optypes and get the instruction opcode.
-    # TODO: If no instruction opcode is found then abort.
-    # TODO: Add new optypes for JMPC_Z and JMPC (so they aren't confused).
-
-    print(f"instr name {instr} optypes {optypes}")
+def encode_instr_opcode(instr: str, optypes: list[InstructionComponent]) -> Instruction | None:
+    #print(f"instr name {instr} optypes {optypes}")
     attribs = [instr]
     attribs.extend([op.type for op in optypes])  # type: ignore
 
-    name = isa.INSTR_ALIAS_LUT[tuple(attribs)]  # type: ignore
+    name = isa.INSTR_ALIAS_LUT.get(tuple(attribs), None)  # type: ignore
+
+    if name is None:
+        return None
+
     opcode = isa.INSTRUCTION_LUT[name]
 
-    print(f"got instruction name {name}, opcode {bin(opcode)}")
+    #print(f"got instruction name {name}, opcode {bin(opcode)}")
     
     return Instruction(name, opcode)
     
@@ -102,7 +128,15 @@ def main(argv: list[str]) -> int:
                   + "consult the CPU ISA documentation.\n> ")
 
     encoded = encode_instr(instr)
-    print(encoded)
+
+    if encoded == -1:
+        return -1
+    
+    # Re-encode as little endian for display.
+    dec = int.to_bytes(encoded, 4, byteorder='little', signed=False)
+    encoded = int.from_bytes(dec, 'big', signed=False)
+
+    print(f"Encoded result (little endian hex): {hex(encoded)}")
     return 0
 
 
--- scripts/include/isa.py
+++ scripts/include/isa.py
@@ -1,4 +1,5 @@
 from .optypes import OPTYPE_CONDITIONAL, OPTYPE_IMMEDIATE, OPTYPE_REGISTER
+from .optypes import REVERSE_OP_LUT
 
 ISA_VERSION_MAJOR = 1
 ISA_VERSION_MINOR = 0
@@ -49,8 +50,7 @@ INSTRUCTION_LUT = {
     "cpuid": 0b00101010         # CPUID DEST_REG
 }
 
-INSTR_ALIAS_LUT = {
-    ("nop",): "lsh",                                                             # NOP                                                           
+INSTR_ALIAS_LUT = {                                                          
     ("add", OPTYPE_REGISTER, OPTYPE_REGISTER, OPTYPE_REGISTER): "add",          # ADD SRC_REG1 SRC_REG2 DEST_REG
     ("add", OPTYPE_REGISTER, OPTYPE_REGISTER): "add_acc",                       # ADD SRC_REG1/DEST_REG SRC_REG2 (add accumulate)
     ("sub", OPTYPE_REGISTER, OPTYPE_REGISTER, OPTYPE_REGISTER): "sub",          # SUB SRC_REG1 SRC_REG2 DEST_REG
@@ -85,15 +85,165 @@ INSTR_ALIAS_LUT = {
     ("jmpz", OPTYPE_CONDITIONAL, OPTYPE_REGISTER, OPTYPE_REGISTER): "jmpc_z",   # JMPZ COND OPERAND_REG1 ADDR_REG
     ("call", OPTYPE_REGISTER, OPTYPE_IMMEDIATE): "call_imm",                    # CALL ADDR_REG IMM_20_BIT_OFFSET
     ("call", OPTYPE_REGISTER, OPTYPE_REGISTER): "call",                         # CALL BASE_ADDR_REG OFFSET_REG
-    ("ret"): "ret",                                                             # RET
+    ("ret",): "ret",                                                             # RET
     ("push", OPTYPE_REGISTER): "push",                                          # PUSH REG
     ("pop", OPTYPE_REGISTER): "pop",                                            # POP REG
-    ("hlt"): "hlt",                                                             # HLT
+    ("hlt",): "hlt",                                                             # HLT
     ("syscall", OPTYPE_REGISTER): "syscall",                                    # SYSCALL ID_REG
     ("sysret"): "sysret",                                                       # SYSRET
     ("systarget", OPTYPE_REGISTER): "systarget",    # DEPRECATED DEPRECATED #   # SYSTARGET HANDLER_ADDRESS_REG
     ("cpuid", OPTYPE_REGISTER): "cpuid"                                         # CPUID DEST_REG
 }
 
-def encode_word():
-    pass
+INSTR_REENCODE_LUT = {
+    "nop": ("lsh", "X0", "0x00")
+}
+
+
+def instruction_def_exists(instr_name: str) -> bool:
+    valid_names = [name[0] for name in INSTR_ALIAS_LUT]
+    return instr_name in valid_names
+
+
+def print_instruction_entries(instr_name: str) -> None:
+    entries = [entry for entry in INSTR_ALIAS_LUT if entry[0] == instr_name]
+
+    for entry in entries:
+        print(f"\t{_entry_as_str(entry)}")  # type: ignore
+
+
+def _entry_as_str(entry: tuple) -> str:
+    out_str = entry[0]
+    ops = " ".join([REVERSE_OP_LUT[op] for op in entry[1:]])
+    return f"{out_str} {ops}"
+
+
+def operands_as_str(operands: list) -> str:
+    return ", ".join([REVERSE_OP_LUT[op.type] for op in operands])
+
+
+def _encode_instr_only(instr) -> int:
+    word = 0 & 0xFFFFFFFF
+    word |= (instr & 0xFFFFFFFF) << 24
+    return word; 
+
+
+# Use loop?
+def _encode_instr_4reg(instr, reg0, reg1, reg2, reg3) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg0.encoded & 0xF) << 20
+    word |= (reg1.encoded & 0xF) << 16
+    word |= (reg2.encoded & 0xF) << 12
+    word |= (reg3.encoded & 0xF) << 8
+    return word
+
+
+def _encode_instr_3reg(instr, reg0, reg1, reg2) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg0.encoded & 0xF) << 20
+    word |= (reg1.encoded & 0xF) << 16
+    word |= (reg2.encoded & 0xF) << 12
+    return word
+
+
+def _encode_instr_2reg(instr, reg0, reg1) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg0.encoded & 0xF) << 20
+    word |= (reg1.encoded & 0xF) << 16
+    return word
+
+
+def _encode_instr_1reg(instr, reg) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg.encoded & 0xF) << 20
+    return word
+
+
+def _encode_instr_reg_imm(instr, reg, imm_20b) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg.encoded & 0xF) << 20
+    word |= (imm_20b.encoded & 0xFFFFFFFF)
+    return word
+
+
+def _encode_instr_2reg_imm(instr, reg0, reg1, imm_16b) -> int:
+    word = _encode_instr_only(instr)
+    word |= (reg0.encoded & 0xF) << 20
+    word |= (reg1.encoded & 0xF) << 16
+    word |= (imm_16b.encoded & 0xFFFFFFFF)
+    return word
+
+
+def encode_word(instr, operands: list) -> int:
+    """
+    Implements the encoding behavior dictated in isa_design.txt
+    
+    -- EXERPT --
+    Instruction decode types:
+        INSTR_ONLY: (no operands).
+            - ADD_FP, SUB_FP, MUL_FP, DIV_FP, RET, HLT, SYSRET
+        INSTR_4REG: (4 register operands; technically just 4-bit operands at the hw level).
+            - JMPC
+        INSTR_3REG: (3 register operands).
+            - ADD, SUB, MUL, DIV, AND, OR, XOR, JMPC_Z
+        INSTR_2REG: (2 register operands).
+            - ADD_ACC, SUB_ACC, MUL_ACC, DIV_ACC, MOV, NOT, LSH, RSH, JMP_REL, CALL
+        INSTR_1REG: (1 register operand).
+            - JMP, PUSH, POP, SYSCALL, SYSTARGET, CPUID
+        INSTR_REG_IMM: (1 register, 1 20-bit immediate).
+            - MOV_IMM, LSH_IMM, RSH_IMM, JMP_IMM, CALL_IMM
+        INSTR_2REG_IMM: (2 register, 1 16-bit immediate).
+            - LDB, STB, LD, ST, MOV_IMM_HIGH
+    -- EXERPT --
+
+    :param instr: Instruction-class object to operate on and encode.
+    :param operands: Operands to encode.
+    """
+
+    encoder_lookup = {
+        "add": _encode_instr_3reg,
+        "add_acc": _encode_instr_2reg,      # ADD SRC_REG1/DEST_REG SRC_REG2 (add accumulate)
+        "sub": _encode_instr_3reg,          # SUB SRC_REG1 SRC_REG2 DEST_REG
+        "sub_acc": _encode_instr_2reg,      # SUB SRC_REG1/DEST_REG SRC_REG2 (sub accumulate)
+        "mul": _encode_instr_3reg,          # MUL SRC_REG1 SRC_REG2 DEST_REG
+        "mul_acc": _encode_instr_2reg,      # MUL SRC_REG1/DEST_REG SRC_REG2 (mul accumulate, not FMA!)
+        "div": _encode_instr_3reg,          # DIV SRC_REG1 SRC_REG2 DEST_REG
+        "div_acc": _encode_instr_2reg,      # DIV SRC_REG1/DEST_REG SRC_REG2 (mul accumulate, not FMA!)
+                                            # NOT IMPLEMENTED: ADD_FP
+                                            # NOT IMPLEMENTED: SUB_FP
+                                            # NOT IMPLEMENTED: MUL_FP
+                                            # NOT IMPLEMENTED: DIV_FP
+        "mov": _encode_instr_2reg,          # MOV SRC_REG DEST_REG
+        "mov_imm": _encode_instr_reg_imm,   # MOV DEST_REG IMM_20_BIT
+        "mov_imm_high": _encode_instr_2reg_imm, # MOVH DEST_REG IMM_16_BIT (MOV HIGH 16 BITS)
+        "ldb": _encode_instr_2reg_imm,      # LDB ADDR_REG DEST_REG IMM_16_BIT_OFFSET
+        "stb": _encode_instr_2reg_imm,      # STB ADDR_REG SRC_REG IMM_16_BIT_OFFSET
+        "ld": _encode_instr_2reg,           # LD ADDR_REG DEST_REG
+        "st": _encode_instr_2reg,           # ST ADDR_REG SRC_REG
+        "and": _encode_instr_3reg,          # AND SRC_REG1 SRC_REG2 DEST_REG
+        "or": _encode_instr_3reg,           # OR SRC_REG1 SRC_REG2 DEST_REG
+        "xor": _encode_instr_3reg,          # XOR SRC_REG1 SRC_REG2 DEST_REG
+        "not": _encode_instr_2reg,          # NOT SRC_REG DEST_REG
+        "lsh": _encode_instr_2reg,          # LSH OPERAND_REG SHIFT_REG
+        "lsh_imm": _encode_instr_reg_imm,   # LSH OPERAND_REG IMM_20_BIT_SHIFT
+        "rsh": _encode_instr_2reg,          # RSH OPERAND_REG SHIFT_REG
+        "rsh_imm": _encode_instr_reg_imm,   # RSH OPERAND_REG IMM_20_BIT_SHIFT
+        "jmp_imm": _encode_instr_reg_imm,   # JMP ADDR_REG IMM_20_BIT_SHIFT
+        "jmp_rel": _encode_instr_2reg,      # JMP BASE_ADDR_REG OFFSET_REG
+        "jmp": _encode_instr_1reg,          # JMP ADDR_REG
+        "jmpc": _encode_instr_4reg,         # JMP COND OPERAND_REG1 OPERAND_REG2 ADDR_REG
+        "jmpc_z": _encode_instr_3reg,       # JMPZ COND OPERAND_REG1 ADDR_REG
+        "call_imm": _encode_instr_reg_imm,  # CALL ADDR_REG IMM_20_BIT_OFFSET
+        "call": _encode_instr_2reg,         # CALL BASE_ADDR_REG OFFSET_REG
+        "ret": _encode_instr_only,          # RET
+        "push": _encode_instr_1reg,         # PUSH REG
+        "pop": _encode_instr_1reg,          # POP REG
+        "hlt": _encode_instr_only,          # HLT
+        "syscall": _encode_instr_1reg,      # SYSCALL ID_REG
+        "sysret": _encode_instr_only,       # SYSRET
+        "systarget": _encode_instr_1reg,    # SYSTARGET HANDLER_ADDRESS_REG
+        "cpuid": _encode_instr_1reg         # CPUID DEST_REG
+    }
+
+    encoder = encoder_lookup[instr.instr]
+    return encoder(instr.opcode, *operands)
